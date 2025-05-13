507cca64f8030d2bb77cd41b731165cf0b30f8a9
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2025-05-13 20:27:50 +0900

    chore: 프로젝트 구성

---
+++ front/src/main/java/com/bubble/giju/GijuApplication.java
@@ -0,0 +1,13 @@
+package com.bubble.giju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GijuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GijuApplication.class, args);
+    }
+
+}
