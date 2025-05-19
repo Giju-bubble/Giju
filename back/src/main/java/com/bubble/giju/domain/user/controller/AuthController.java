e4fad7ccea74d2d43632af41b38b465baa0cd7fc
Seonjun Kim <kimsunjun1221@gmail.com>
2025-05-19 17:21:27 +0900

    feat: 회원가입 성공 응답

--- back/src/main/java/com/bubble/giju/domain/user/controller/AuthController.java
+++ back/src/main/java/com/bubble/giju/domain/user/controller/AuthController.java
@@ -3,7 +3,9 @@ package com.bubble.giju.domain.user.controller;
 import com.bubble.giju.domain.user.dto.UserCreateRequest;
 import com.bubble.giju.domain.user.entity.User;
 import com.bubble.giju.domain.user.service.UserService;
+import com.bubble.giju.global.config.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,12 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public String createUser(@RequestBody UserCreateRequest userCreateRequest) {
+    public ResponseEntity<ApiResponse> createUser(@RequestBody UserCreateRequest userCreateRequest) {
         System.out.println(userCreateRequest);
 
         userService.save(userCreateRequest);
 
-        return "success";
+        ApiResponse<String> apiResponse = ApiResponse.success("회원가입 성공", null);
+        return ResponseEntity.ok(apiResponse);
     }
 }
