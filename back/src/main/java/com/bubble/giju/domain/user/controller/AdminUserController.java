fba75e625b5d33bf7bb2f4358d4356bf2902565d
Seonjun Kim <kimsunjun1221@gmail.com>
2025-05-22 23:40:41 +0900

    fix: 소냐큐브 워닝 수정

--- back/src/main/java/com/bubble/giju/domain/user/controller/AdminUserController.java
+++ back/src/main/java/com/bubble/giju/domain/user/controller/AdminUserController.java
@@ -1,10 +1,8 @@
 package com.bubble.giju.domain.user.controller;
 
 import com.bubble.giju.domain.user.dto.UserDto;
-import com.bubble.giju.domain.user.entity.User;
 import com.bubble.giju.domain.user.service.UserAdminService;
 import com.bubble.giju.domain.user.service.UserService;
-import com.bubble.giju.domain.user.service.impl.UserServiceImpl;
 import com.bubble.giju.global.config.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,7 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Admin - User", description = "관리자 - 사용자 관련 API")
-@PreAuthorize("ADMIN")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
 @RestController
@@ -30,9 +28,8 @@ public class AdminUserController {
     @Operation(summary = "모든 사용자 아이디 리스트를 불러옵니다.", description = "UUID userID 리스트입니다, 가져올 페이지번호와 갯수를 입력합니다.")
     @GetMapping("/users")
     public ResponseEntity<ApiResponse<?>> getUsers(@RequestParam(required = false, defaultValue = "1") int page,
-                         @RequestParam(required = false, defaultValue = "5") int size,
-                         Pageable pageable) {
-        pageable = PageRequest.of(page - 1, size);
+                         @RequestParam(required = false, defaultValue = "5") int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
 
         Page<String> allUserIds = userAdminService.getAllUsers(pageable);
         ApiResponse<Page<String>> apiResponse = ApiResponse.success("전체 유저ID 목록입니다.", allUserIds);
