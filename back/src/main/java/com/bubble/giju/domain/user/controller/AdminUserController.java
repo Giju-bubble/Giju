e1002cf49e4f0689f4b77a7a5f7c123bf6917fcd
Seonjun Kim <kimsunjun1221@gmail.com>
2025-05-22 18:14:08 +0900

    feat: admin - 회원목록, 특정 회원 불러오기 기능 추가

--- back/src/main/java/com/bubble/giju/domain/user/controller/AdminUserController.java
+++ back/src/main/java/com/bubble/giju/domain/user/controller/AdminUserController.java
@@ -1,7 +1,10 @@
 package com.bubble.giju.domain.user.controller;
 
+import com.bubble.giju.domain.user.dto.UserDto;
 import com.bubble.giju.domain.user.entity.User;
 import com.bubble.giju.domain.user.service.UserAdminService;
+import com.bubble.giju.domain.user.service.UserService;
+import com.bubble.giju.domain.user.service.impl.UserServiceImpl;
 import com.bubble.giju.global.config.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdminUserController {
 
     private final UserAdminService userAdminService;
+    private final UserService userService;
 
     @GetMapping("/users")
     public ResponseEntity<ApiResponse<?>> getUsers(@RequestParam(required = false, defaultValue = "1") int page,
@@ -27,13 +31,17 @@ public class AdminUserController {
         pageable = PageRequest.of(page - 1, size);
 
         Page<String> allUserIds = userAdminService.getAllUsers(pageable);
-        ApiResponse<Page<String>> apiResponse = ApiResponse.success("전체 유저입니다.", allUserIds);
+        ApiResponse<Page<String>> apiResponse = ApiResponse.success("전체 유저ID 목록입니다.", allUserIds);
 
          return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
     @GetMapping("/users/{userId}")
-    public void getUser(@PathVariable String userId) {
+    public ResponseEntity<ApiResponse<UserDto.Response>> getUser(@PathVariable String userId) {
+        UserDto.Response response = userService.find(userId);
 
+        ApiResponse<UserDto.Response> apiResponse = ApiResponse.success("Admin - 회원불러오기 성공", response);
+
+        return ResponseEntity.ok(apiResponse);
     }
 }
