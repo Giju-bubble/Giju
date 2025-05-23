cd03ef893a1d2cc40aba8ca0b0ac60857f578b3c
Seonjun Kim <kimsunjun1221@gmail.com>
2025-05-23 15:42:22 +0900

    feat: address 수정 중

--- back/src/main/java/com/bubble/giju/domain/address/controller/AddressController.java
+++ back/src/main/java/com/bubble/giju/domain/address/controller/AddressController.java
@@ -3,6 +3,7 @@ package com.bubble.giju.domain.address.controller;
 import com.bubble.giju.domain.address.dto.AddressDto;
 import com.bubble.giju.domain.address.service.AddressService;
 import com.bubble.giju.domain.user.dto.CustomPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,18 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/address")
 @RestController
 public class AddressController {
 
     private final AddressService addressService;
 
-    @GetMapping("")
+    @Operation(summary = "회원의 모든 주소가져오기", description = "회원의 모든 주소를 가져옵니다.")
+    @GetMapping("/")
     public void getAddress(@AuthenticationPrincipal CustomPrincipal customPrincipal, @RequestBody AddressDto.Request request) {
-
-
         addressService.createAddress(customPrincipal.getUserId(), request);
-
-
     }
+
+
 }
