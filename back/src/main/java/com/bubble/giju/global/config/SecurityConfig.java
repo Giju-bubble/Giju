29a3c22547a4dac4ca37b52c1f5eb41216f819c4
jongbinchoi <jongbin985@naver.com>
2025-06-25 21:27:38 +0900

    fix: ecs-task-def.json giju-task로 변경

--- back/src/main/java/com/bubble/giju/global/config/SecurityConfig.java
+++ back/src/main/java/com/bubble/giju/global/config/SecurityConfig.java
@@ -98,7 +98,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/api/auth/**", "/oauth2/**", "/error",
                                 "/api/categories", "/api/rankings", "/api/payment/**", "/api/drink/**", "/api/drinks",
-                                "/toss/**", "/api/regions", "/api/reviews/drinks/**").permitAll()
+                                "/toss/**", "/api/regions", "/api/reviews/drinks/**", "/health").permitAll()
                         .requestMatchers("/swagger-ui/**",
                                 "/api/swagger-config/**",
                                 "/h2-console/**",
